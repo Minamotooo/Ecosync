715599d2e653964001679c6d34f01103295da464
Minamotooo <aurchichowdhury07@gmail.com>
2024-09-07 00:32:57 +0600

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Map from './components/Map'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Map />
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/components/Map.jsx
@@ -0,0 +1,9 @@
+export default function Map() {
+
+    return(
+        <div>
+            ekhane kaaj kor, ami copy paste kortesi.
+        </div>
+    )
+    
+}
